3f983024e4636194018456d772bb75c15d5fb033
AmitChigare <amit.gokakfalls17@gmail.com>
2023-02-08 22:03:52 +0530

    Finshed Team model and admin panel

--- carzone/settings.py
+++ carzone/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,8 +77,13 @@ WSGI_APPLICATION = "carzone.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        # "ENGINE": "django.db.backends.sqlite3",
+        # "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "carzone_db",
+        "USER": "postgres",
+        "PASSWORD": "Passpass@1",
+        "HOST": "localhost",
     }
 }
 
@@ -124,6 +130,12 @@ STATICFILES_DIRS = [
     # os.path.join(BASE_DIR, "carzone/static"),
     "carzone/static",
 ]
+
+
+# Added Manually
+# Media Files Configuration
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
