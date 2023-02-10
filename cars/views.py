9ddf86568b62ad00778f7c6eec928ce0d543e82f
AmitChigare <amit.gokakfalls17@gmail.com>
2023-02-10 18:08:57 +0530

    Completed search functionalities and admin panel (8)

--- cars/views.py
+++ cars/views.py
@@ -1,5 +1,60 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Car
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 # Create your views here.
 def cars(request):
-    return render(request, "cars/cars.html")
+    cars = Car.objects.all().order_by("-created_date")
+    paginator = Paginator(cars, 4)
+    page = request.GET.get("page")
+    paged_cars = paginator.get_page(page)
+    context = {
+        "cars": paged_cars,
+    }
+    return render(request, "cars/cars.html", context)
+
+
+def car_detail(request, id):
+    single_car = get_object_or_404(Car, pk=id)
+    context = {
+        "single_car": single_car,
+    }
+    return render(request, "cars/car_detail.html", context)
+
+
+def search(request):
+    keyword = ""
+    cars = Car.objects.all().order_by("-created_date")
+    if "keyword" in request.GET:
+        keyword = request.GET["keyword"]
+        if keyword:
+            cars = cars.filter(description__icontains=keyword)
+
+    if "city" in request.GET:
+        city = request.GET["city"]
+        if city:
+            cars = cars.filter(city__iexact=city)
+
+    if "year" in request.GET:
+        year = request.GET["year"]
+        if year:
+            cars = cars.filter(year__iexact=year)
+
+    if "model" in request.GET:
+        model = request.GET["model"]
+        if model:
+            cars = cars.filter(model__iexact=model)
+
+    if "body_style" in request.GET:
+        body_style = request.GET["body_style"]
+        if keyword:
+            cars = cars.filter(body_style__iexact=body_style)
+
+    if "min_price" in request.GET:
+        min_price = request.GET["min_price"]
+        max_price = request.GET["max_price"]
+        if max_price:
+            cars = cars.filter(price__gte=min_price, price__lte=max_price)
+
+    context = {"cars": cars, "keyword": keyword}
+    return render(request, "cars/search.html", context)
